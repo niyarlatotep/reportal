b863efcf913da93175fbdf1320eaefec8b53189b
niarlatotep <caimac@yandex.ru>
2019-04-23 09:32:40 +0300

    Project name not unique

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -12,11 +12,14 @@ projectRouter.post('/project', async (req, res) => {
     if (!req.body){
         return res.status(400).send('Request body is missing');
     }
+    console.log(req.body);
     const model = new ProjectModel(req.body);
+    console.log(model);
     try {
         const doc = await model.save();
         res.sendStatus(200);
     } catch (err) {
+        console.error(err);
         res.status(500).json(err);
     }
 });
